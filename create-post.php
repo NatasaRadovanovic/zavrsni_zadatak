7d047bc536c641e03ff99029db5ff91d0072d6f1
Natasa Radovanovic <natasar.radovanovic@gmail.com>
2018-06-07 21:52:43 +0200

    kreiranje posta

--- create-post.php
+++ create-post.php
@@ -1,8 +1,34 @@
 <?php
-    
+    include('include/connection.php');
     include('include/header.php');
-    include('include/sidebar.php');
-    include('include/footer.php');
+  
+        
+         
+    if(!isset($_POST['sendPost'])){
+        header("Location:create.php");
+    }else{
+        $author = $_POST['author'];
+        $title = $_POST['title'];
+        $newPost = $_POST['newPost'];
 
-?>
+        if(empty($author) || empty ($title) || empty ($newPost)){
+            header("location:create.php?error=1");
+        }else{
+            
+         
+            $sql = "INSERT INTO posts (Title,Body,Author) VALUES ('$title','$newPost','$author')";
+    
+            $statement = $connection->prepare($sql);
+            $statement->execute();
+    
+           header("Location:index.php");
+        }
+    }
+
+      
+    ?>
+    
+    
+
+
 
